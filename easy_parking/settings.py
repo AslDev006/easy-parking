e0429491dfc62fd92798cb110facdcfbcaf4f94c
AslDev006 <asldev006@gmail.com>
2025-05-22 01:03:47 +0500

    asdfsfsd

--- easy_parking/settings.py
+++ easy_parking/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
