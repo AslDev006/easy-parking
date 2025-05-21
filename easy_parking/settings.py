61cb1a712f883a75a160e7db7296a83bd97fa9dc
AslDev006 <asldev006@gmail.com>
2025-05-22 00:38:56 +0500

    asdasdasd

--- easy_parking/settings.py
+++ easy_parking/settings.py
@@ -93,5 +93,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # Collect static files here
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "https://localhost",
+    "https://127.0.0.1",
+    "https://easy-parking.onrender.com",
+]
